bd8be384252ad8f300d582458a7d19655f1b4b70
amber-0 <105370349+amber-0@users.noreply.github.com>
2023-01-13 00:46:51 +0900

    Add files via upload

---
+++ database/migrations/2023_01_08_181236_add_title_to_contact_forms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contact_forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('email, 255');
+            $table->longText('url')->nullable();
+            $table->boolean('gender');
+            $table->tinyInteger('age');
+            $table->string('contact', 200);
+            $table->timestamps();
+        });
+    }
+};
+
+
+
